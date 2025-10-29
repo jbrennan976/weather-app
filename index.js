ef4964c09397721e646108f94633e38b3fc16b26
jbrennan976 <jbrennan9763@gmail.com>
2025-10-29 19:18:33 -0400

    Configured javascript and html to allow user to search for weather by city from backend

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-function formSubmit(){
-    let city = document.getElementById("city").value;
-    console.log(city)
-    
+document.getElementById("myForm").addEventListener("submit", async (event) => {
+    event.preventDefault()
+    let city = document.getElementById("city").value
+    city = city.trim().toLowerCase()
+    await fetchWeather(city)
+    document.getElementById("myForm").reset()
+});
 
+async function fetchWeather(city){
+    try {
+        const response = await fetch(`http://127.0.0.1:8000/weather?city=${city}`)
+
+        if (!response.ok){
+            throw new Error(`An error occured getting weather for ${city}`)
+        }
+        const data = await response.json()
+        console.log(data)
+    }
+
+    catch (error) {
+        console.error(error)
+    }
 }
